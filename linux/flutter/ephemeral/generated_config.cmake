c2aa60f9348f6ab01bd1e9f959baceec65586b7a
李亚楠 <liyanan@example.com>
2026-04-24 23:50:34 +0800

    Fix CMake configuration for Linux build

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/Liyanan/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/Liyanan/.openclaw/workspace/P2P-IM-portal-desktop" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/Liyanan/flutter"
+  "PROJECT_DIR=/home/Liyanan/.openclaw/workspace/P2P-IM-portal-desktop"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWdpdEBnaXRodWIuY29tOmZsdXR0ZXIvZmx1dHRlci5naXQ=,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049Y2MwNzM0YWM3MQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTlhYTU4NGZkZg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS41"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/Liyanan/.openclaw/workspace/P2P-IM-portal-desktop/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/Liyanan/.openclaw/workspace/P2P-IM-portal-desktop/lib/main.dart"
+)
